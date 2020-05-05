b26cbe5a891b2957b6003be32764a380c3fa9547
lowlandjuju <mp@fixedabode.cc>
2020-05-06 00:28:52 +0200

    Missing dummy app

---
+++ test/dummy/config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  mount SettingsUi::Engine => "/settings_ui"
+end
---
+++ test/settings_ui_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SettingsUi::Test < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, SettingsUi
+  end
+end
